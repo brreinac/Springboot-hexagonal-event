9bf9e22a21ae31e1c76bae11b0a73a4534f50841
Brayan Reina <brreinac@poligran.edu.co>
2026-09-20 21:14:42 -0500

    chore: inicializa proyecto Spring Boot

---
+++ src/main/java/com/segurosbolivar/polizas/GestionPolizasApplication.java
@@ -0,0 +1,12 @@
+package com.segurosbolivar.polizas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPolizasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GestionPolizasApplication.class, args);
+    }
+}
